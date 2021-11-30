225df67bbe40cf411807fddb335fc3cf2b460755
T. Parmentier <66419209+TParm@users.noreply.github.com>
2021-11-30 09:50:03 +0100

    Changing directory names

---
+++ IO_Logging/build/CMakeFiles/IO_Logging.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Documents/opdrachten/RPI_IO_Logging_TC74/IO_Logging/IO_Logging.c" "/home/pi/Documents/opdrachten/RPI_IO_Logging_TC74/IO_Logging/build/CMakeFiles/IO_Logging.dir/IO_Logging.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/mariadb"
+  "/usr/include/mariadb/mysql"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ IO_Logging/build/CMakeFiles/IO_Logging.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IO_Logging.dir/IO_Logging.c.o"
+  "IO_Logging"
+  "IO_Logging.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IO_Logging.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
